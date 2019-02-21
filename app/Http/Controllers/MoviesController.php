71345b3abc219b500792e44364e0b33491ffb1c5
Thiago Augusto <thiagoeu@gmail.com>
2019-02-21 12:46:06 -0300

    fix repeated movies

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -8,8 +8,11 @@ class MoviesController extends Controller
 {
     public function index()
     {
-        $movies = Movie::select('movies.*')
+        $columns = ['movies.id', 'movies.title', 'movies.original_title', 'movies.year', 'movies.slug', 'movies.category', 'movies.genre', 'movies.description', 'movies.poster_path', 'movies.backdrop_path'];
+
+        $movies = Movie::select($columns)
             ->join('threads', 'movies.id', '=', 'threads.movie_id')
+            ->groupBy($columns)
             ->orderBy('threads.id', 'desc')
             ->paginate(24);
 
