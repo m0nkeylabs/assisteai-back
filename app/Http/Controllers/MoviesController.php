e2961f1cd174da8ffd9cae0bad340c50f9b11d3d
Thiago Augusto <thiagoeu@gmail.com>
2019-02-25 12:35:10 -0300

    implement search in movies

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -3,19 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Movie;
+use Illuminate\Http\Request;
 
 class MoviesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $query = $request->input('q');
+
         $columns = ['movies.id', 'movies.title', 'movies.original_title', 'movies.year', 'movies.slug', 'movies.category', 'movies.genre', 'movies.description', 'movies.poster_path', 'movies.backdrop_path'];
 
         $movies = Movie::select($columns)
             ->join('threads', 'movies.id', '=', 'threads.movie_id')
             ->groupBy($columns)
-            ->orderBy('threads.created_at', 'desc')
-            ->paginate(24);
+            ->orderBy('threads.created_at', 'desc');
 
-        return response()->json($movies);
+        if ($query) {
+            $movies->where('title', 'like', '%' . $query . '%')
+                ->orWhere('original_title', 'like', '%' . $query . '%')
+                ->orWhere('year', '=', $query)
+                ->orWhere('description', 'like', '%' . $query . '%');
+        }
+
+        return response()->json($movies->paginate());
     }
 }
