64c9407c92b86c9598026e9d840f75ce74e766ba
Thiago Augusto <thiagoeu@gmail.com>
2019-02-25 11:23:33 -0300

    fix /auth/refresh endpoint wrongly using auth middleware

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ $router->get('/', 'HomeController@index');
 // auth
 $router->group(['prefix' => 'auth'], function () use ($router) {
     $router->get('me', ['uses' => 'AuthController@me', 'middleware' => 'auth']);
-    $router->get('refresh', ['uses' => 'AuthController@refresh', 'middleware' => 'auth']);
     $router->get('logout', ['uses' => 'AuthController@logout', 'middleware' => 'auth']);
+    $router->get('refresh', ['uses' => 'AuthController@refresh']);
 
     // login
     $router->post('login', 'AuthController@authenticate');
